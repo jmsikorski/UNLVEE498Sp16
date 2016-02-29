772bb868d440433f117ee289d6a35173ccbd7744
jmsikorski <sikorsk4@unlv.nevada.edu>
2016-02-28 18:02:43 -0800

    updated temp read

--- Senior Design/Senior Design/main.c
+++ Senior Design/Senior Design/main.c
@@ -1,37 +1,35 @@
 /*
  * Senior Design.c
  *
- * Created: 2/24/2016 9:32:01 AM
+ * Created: 2/27/2016 7:15:24 PM
  * Author : jmsikorski
  */ 
-
+#define F_CPU 16000000UL
 #include <avr/io.h>
-
-void read_temp(int t);
+#include <util/delay.h>
 
 int main(void)
 {
+    /* Replace with your application code */
+	DDRB = 0xFF;
+	DDRD = 0XFF;
+	DDRF = 0;
+	ADCSRA = 0x87;
+	ADMUX = 0xC0;
 	int temp = 0;
-	while(1)
-	{
-		read_temp(temp);
-		DDRD = 0xFF;
-		DDRB = 0xFF;
+	
+    while (1) 
+    {
+		_delay_ms(200);
+		ADCSRA |= (1<<ADSC);
+		while((ADCSRA & (1<<ADIF))==0);
+		temp = ADC;
+		temp *= 9;
+		temp /= 20;
+		temp -= 58;
 		PORTD = temp % 256;
 		PORTB = temp / 256;
-	}
+    }
 	return 0;
-}
-
-void read_temp(int t)
-{
-	DDRF &= (0<<ADC0D);	//make Port F[0] an input for ADC input
-	DIDR0 &= (1<<ADC0D);//disable digital input buffer on F[0]
-	ADCSRA = 0x87;		//make ADC enable and select ck/128
-	ADMUX = 0xC0;		//2.56V Vref, ADC0 single ended input
-						//data will be right-justified
-	ADCSRA |= (1<<ADSC);	//start conversion
-	while((ADCSRA & (1<<ADIF))==0);	//wait for conversion to finish
-	t = ADC;
 }
 
